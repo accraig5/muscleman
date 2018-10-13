abcbeac0590b9ae675948650dd95b25427c333ef
Aaron Craig <12929283+accraig5@users.noreply.github.com>
2018-10-13 10:02:21 -0400

    Created Spring Project With Gradle Configured

---
+++ src/main/java/com/example/muscleman/MusclemanApplication.java
@@ -0,0 +1,12 @@
+package com.example.muscleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclemanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusclemanApplication.class, args);
+    }
+}
